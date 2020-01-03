9ab2b9221edffc56cb775fd7cc0b07f88943e3cd
nsa.axelor <nsa.axelor@gmail.com>
2020-01-03 19:17:48 +0530

    chart view and kanban view done

--- modules/axelor-student/src/main/java/com/axelor/app/student/web/StudentController.java
+++ modules/axelor-student/src/main/java/com/axelor/app/student/web/StudentController.java
@@ -3,10 +3,16 @@ package com.axelor.app.student.web;
 import com.axelor.app.student.db.Student;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class StudentController {
 
-  public void setStudentAge(ActionRequest request, ActionResponse response) {
+  public void countStudentAge(ActionRequest request, ActionResponse response) {
     Student student = request.getContext().asType(Student.class);
+    LocalDate bdate = student.getBdate();
+    LocalDate today = LocalDate.now();
+    Period p = Period.between(bdate, today);
+    response.setValue("age", p.getYears());
   }
 }
