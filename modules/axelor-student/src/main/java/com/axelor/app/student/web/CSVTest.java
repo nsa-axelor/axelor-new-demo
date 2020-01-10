019bf5a2a41e0a373b75eb5c96eb8d93b1e06d0e
nsa.axelor <nsa.axelor@gmail.com>
2020-01-10 18:59:00 +0530

    csv fix now birt report

--- modules/axelor-student/src/main/java/com/axelor/app/student/web/CSVTest.java
+++ modules/axelor-student/src/main/java/com/axelor/app/student/web/CSVTest.java
@@ -1,17 +1,5 @@
 package com.axelor.app.student.web;
 
-import java.io.File;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.commons.io.IOUtils;
-
 import com.axelor.data.ImportTask;
 import com.axelor.data.Importer;
 import com.axelor.data.Listener;
@@ -19,70 +7,76 @@ import com.axelor.data.csv.CSVImporter;
 import com.axelor.db.Model;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CSVTest {
-	
-	private Logger log = LoggerFactory.getLogger(getClass());
 
-	public void setSports(ActionRequest request , ActionResponse response) throws IOException {
-		File configFile = getConfigFile();
-		
-		final List<Model> records = new ArrayList<>();
-		
-		Importer importer = new CSVImporter(configFile.getAbsolutePath(),getDataCsvFile());
-		importer.run();
-		importer.addListener(new Listener() {
-			
-			@Override
-		      public void imported(Model bean) {
-		        log.info("Bean saved : {}(id={})",
-		            bean.getClass().getSimpleName(),
-		            bean.getId());
-		        
-		        System.err.println("Bean saved : {}(id={})"+
-		            bean.getClass().getSimpleName()+
-		            bean.getId());
-		        
-		        records.add(bean);
-		      }
+  private Logger log = LoggerFactory.getLogger(getClass());
 
-		      @Override
-		      public void imported(Integer total, Integer success) {
-		        log.info("Record (total): " + total);
-		        log.info("Record (success): " + success);
-		        System.err.println("TOTAL: "+total+"  SUCCESS: "+success);
-		      }
+  public void setSports(ActionRequest request, ActionResponse response) throws IOException {
+    File configFile = getConfigFile();
 
-		      @Override
-		      public void handle(Model bean, Exception e) {
-		    	  System.err.println("HANDLE");
-		      }
-		});
-		
-		importer.run(new ImportTask() {
-			
-			@Override
-			public void configure() throws IOException {
-				// TODO Auto-generated method stub
-				System.err.println("CONFIGURE");
-			}
-		});
-		response.setReload(true);
-	}
+    final List<Model> records = new ArrayList<>();
 
-	public File getConfigFile() throws IOException {
-		File configFile = File.createTempFile("csv-config", ".xml");
-		InputStream inputStream = this.getClass().getResourceAsStream("/data-demo/input-config2.xml");
-		FileOutputStream fileOutputStream = new FileOutputStream(configFile);
-		IOUtils.copy(inputStream, fileOutputStream);
-		return configFile;
-	}
-	
-	public String getDataCsvFile() {
-		URL url = this.getClass().getResource("/data-demo/input/");
-		return url.getPath();
-	}
-	
-	
-  
+    Importer importer = new CSVImporter(configFile.getAbsolutePath(), getDataCsvFile());
+    importer.run();
+    importer.addListener(
+        new Listener() {
+
+          @Override
+          public void imported(Model bean) {
+            log.info("Bean saved : {}(id={})", bean.getClass().getSimpleName(), bean.getId());
+
+            System.err.println(
+                "Bean saved : {}(id={})" + bean.getClass().getSimpleName() + bean.getId());
+
+            records.add(bean);
+          }
+
+          @Override
+          public void imported(Integer total, Integer success) {
+            log.info("Record (total): " + total);
+            log.info("Record (success): " + success);
+            System.err.println("TOTAL: " + total + "  SUCCESS: " + success);
+          }
+
+          @Override
+          public void handle(Model bean, Exception e) {
+            System.err.println("HANDLE");
+          }
+        });
+
+    importer.run(
+        new ImportTask() {
+
+          @Override
+          public void configure() throws IOException {
+            // TODO Auto-generated method stub
+            System.err.println("CONFIGURE");
+          }
+        });
+    response.setReload(true);
+  }
+
+  public File getConfigFile() throws IOException {
+    File configFile = File.createTempFile("csv-config", ".xml");
+    InputStream inputStream = this.getClass().getResourceAsStream("/data-demo/input-config2.xml");
+    FileOutputStream fileOutputStream = new FileOutputStream(configFile);
+    IOUtils.copy(inputStream, fileOutputStream);
+    return configFile;
+  }
+
+  public String getDataCsvFile() {
+    URL url = this.getClass().getResource("/data-demo/input/");
+    return url.getPath();
+  }
 }
--- modules/axelor-student/src/main/java/com/axelor/app/student/web/SportsController.java
+++ modules/axelor-student/src/main/java/com/axelor/app/student/web/SportsController.java
@@ -1,23 +1,22 @@
 package com.axelor.app.student.web;
 
-import java.util.Map;
-
 import com.axelor.app.student.db.Sports;
 import com.axelor.db.JPA;
 import com.axelor.db.JpaSupport;
+import java.util.Map;
 
 public class SportsController extends JpaSupport {
 
-	public Object checkForStudentBinding(Object bean, Map<String, Object> context) {
-		assert bean instanceof Sports;
-		Sports sp = (Sports) bean;
+  public Object checkForStudentBinding(Object bean, Map<String, Object> context) {
+    assert bean instanceof Sports;
+    //		Sports sp = (Sports) bean;
 
-//		Student s = JPA.all(Student.class).filter("self.name = ?",sp.getStudent()).fetchOne();
+    //		Student s = JPA.all(Student.class).filter("self.name = ?",sp.getStudent()).fetchOne();
 
-		long count = JPA.all(Sports.class).count();
-		System.err.println("COUNT-SPORTS: " + count);
-		assert count > 1;
+    long count = JPA.all(Sports.class).count();
+    System.out.println("COUNT-SPORTS: " + count);
+    assert count > 1;
 
-		return bean;
-	}
+    return bean;
+  }
 }
